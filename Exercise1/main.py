976b05f047a943d3d403b0d70720cbac6f76add8
Hendrik Vloet <hendrik.vloet@gmx.de>
2018-05-04 23:49:00 +0200

    added main.py and report

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -1,242 +1,262 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 from __future__ import print_function, division
-import os
+
 import torch
-import pandas as pd
-from skimage import io, transform
-import cv2
-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
 import numpy as np
-import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets, models, transforms
+
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-
-# Ignore warnings
-import warnings
-warnings.filterwarnings("ignore")
-
-# plt.ion()   # interactive mode
-
-### Global Functions ###########################################################
+from torch.utils.data.sampler import  WeightedRandomSampler
 
 
-def show_landmarks(image, landmarks):
-    # extract coordinates landmakrs in a more reader friendly version
-    x1, y1, x2, y2 = landmarks[2:6]
-    plt.imshow(image)
-    # plot edges of landmarks
-    plt.plot([x1,x2], [y1,y1], c='r', marker='o')
-    plt.plot([x1,x2], [y2,y2], c='r', marker='o')
-    plt.plot([x1,x1], [y1,y2], c='r', marker='o')
-    plt.plot([x2,x2], [y1,y2], c='r', marker='o')
-    plt.pause(0.001)  # pause a bit so that plots are updated
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
 
-class StreetSignLandmarkDataset(Dataset):
-    """Street Sign Landmark Dataset
-        Args:
-            csv_file (string): Path to the csv file with annotations
-            root_dir (string): Directory with the images
-            transform (Callables, optional): optional transform to be applied
-                on a sample.
-        """
-    def __init__(self, csv_file, root_dir, transform = None):
-        self.landmarks_frame = pd.read_csv(csv_file, sep=';')
-        self.root_dir = root_dir
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.landmarks_frame)
-
-    def __getitem__(self, idx):
-        self.img_name = os.path.join(self.root_dir, self.landmarks_frame.iloc[idx,0])
-        image = io.imread(self.img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:].as_matrix()
-        landmarks = landmarks.astype('int').reshape(-1,7)
-        landmarks = landmarks.squeeze()
-        sample = {'image' : image, 'landmarks': landmarks}
-
-        if self.transform:
-            sample = self.transform(sample)
-        return sample
-
-class Rescale(object):
-    """Rescale the image in a sample to a given size"""
-    """
-    Args:
-        output_size (tuples or int): Desired output size. If tuple, output is
-            matched to output_size. If int, smaller of image edges is matched
-            to output_size keeping aspect ratio the same.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        self.output_size = output_size
-
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
-
-        h, w = image.shape[:2]
-        if isinstance(self.output_size, int):
-            if h > w:
-                new_h, new_w = self.output_size * h / w, self.output_size
-            else:
-                new_h, new_w = self.output_size, self.output_size * h / w
-        else:
-            new_h, new_w = self.output_size
-        new_h, new_w = int(new_h), int(new_w)
-        img = transform.resize(image, (new_h, new_w))
-
-        # h and w are swapped for landmarks because for images,
-        # x and y axes are axis 1 and 0 respectively
-        # print(landmarks)
-        # print(new_w, w, new_h, h)
-        # landmarks[:] = landmarks[:] * [new_w / w, new_h / h]
-
-        landmarks[0] = landmarks[0] * (new_w / w)
-        landmarks[1] = landmarks[1] * (new_h / h)
-        # print(landmarks[2])
-        landmarks[2] = landmarks[2] * (new_w / w)
-        landmarks[3] = landmarks[3] * (new_h / h)
-
-        landmarks[4] = landmarks[4] * (new_w / w)
-        landmarks[5] = landmarks[5] * (new_h / h)
+device = torch.device("cuda")
 
 
-        # print(landmarks)
 
-        return {'image': img, 'landmarks': landmarks}
-
-
-class RandomCrop(object):
-    """Crop randomly the image in a sample.
-
-    Args:
-        output_size (tuple or int): Desired output size. If int, square crop
-            is made.
-    """
-
-    def __init__(self, output_size):
-        assert isinstance(output_size, (int, tuple))
-        if isinstance(output_size, int):
-            self.output_size = (output_size, output_size)
-        else:
-            assert len(output_size) == 2
-            self.output_size = output_size
-
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
-
-        h, w = image.shape[:2]
-        new_h, new_w = self.output_size
-
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
-
-        image = image[top: top + new_h,
-                      left: left + new_w]
-        print(h, new_h, top)
-        print(w, new_w, left)
-        print(landmarks)
-        # # landmarks = landmarks - [left, top]
-        landmarks[0] -= new_w
-        landmarks[1] -= new_h
-        # print(landmarks[2])
-        landmarks[2] = (landmarks[2]  - (left + new_w)) * (new_w / w)
-        landmarks[3] = (landmarks[3]  - (top + new_h)) * (new_h / h)
-        landmarks[4] = (landmarks[4]  - (left + new_w)) * (new_w / w)
-        landmarks[5] = (landmarks[5]  - (top + new_h)) * (new_h / h)
-        #
-        # landmarks[3] = landmarks[3] * (new_h / h) - (top + new_h)
-        #
-        # landmarks[4] = landmarks[4] * (new_w / w) - (left + new_w)
-        # landmarks[5] = landmarks[5] * (new_h / h) - (top + new_h)
-
-        print(landmarks)
-        return {'image': image, 'landmarks': landmarks}
+data_dir = 'TrainingSet'
 
 
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
+### some statics
 
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
-
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
-        image = image.transpose((2, 0, 1))
-        print(image.shape)
-        return {'image': torch.from_numpy(image),
-                'landmarks': torch.from_numpy(landmarks)}
-
-################################################################################
+BATCH_SIZE = 256
 
 
-# device = torch.device("cuda")
-#
-# landmarks_frame = pd.read_csv('TrainingSet/00000/GT-00000.csv',sep = ';')
-# n = 2
-#
-#
-# img_name = landmarks_frame.iloc[n, 0]
-# landmarks = landmarks_frame.iloc[n,1:].as_matrix()
-# landmarks = landmarks.astype('int').reshape(-1,7)
-# landmarks = landmarks.squeeze()
-# print("Image Name: {}".format(img_name))
-# print("Landmaks Shape: {}".format(landmarks.shape))
-# print("First 4 Landmarks: {}".format(landmarks[:4]))
-# print("All Landmarks: {}".format(landmarks[:]))
-#
-#
-# show_landmarks(io.imread(os.path.join('TrainingSet/00000', img_name)), landmarks)
-#
+# depth of first filter
+depth_1 = 60
 
-sign_dataset = StreetSignLandmarkDataset(
-                    csv_file = 'TrainingSet/00000/GT-00000.csv',
-                    root_dir = 'TrainingSet/00000')
+# depth of second filter
+depth_2 = 100
+
+# parameters for flattened array
+num_param = 7*7*depth_2
+
+# training epochs
+num_train_epochs = 10
+
+# learning rate
+lr= 0.1
+
+# logging lists
+train_loss_epoch = []
+list_val_loss = []
+list_accuracy = []
 
 
-transformed_dataset = StreetSignLandmarkDataset(csv_file = 'TrainingSet/00000/GT-00000.csv',
-root_dir = 'TrainingSet/00000', transform=transforms.Compose([Rescale((32,32)),ToTensor()]))
-
-dataloader = DataLoader(transformed_dataset, batch_size=1, shuffle=True, num_workers=4)
 
 
-def show_landmarks_batch(sample_batched):
-    """Show image with landmarks for a batch of samples."""
-    images_batch, landmarks_batch = \
-            sample_batched['image'], sample_batched['landmarks']
-    batch_size = len(images_batch)
-    im_size = images_batch.size(2)
 
-    grid = utils.make_grid(images_batch)
-    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+### Neural Network Class
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, depth_1, kernel_size = 3, padding = 1, stride = 1)
+        self.conv2 = nn.Conv2d(depth_1, depth_2, kernel_size = 3, padding = 0, stride = 1)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(num_param, 512)
+        self.fc2 = nn.Linear(512, 43)
 
-    print(landmarks_batch)
-    for i in range(batch_size):
-        # plt.scatter(landmarks_batch[i, 0].numpy() + i * im_size,
-        #             landmarks_batch[i, 1].numpy(),
-        #             s=10, marker='.', c='r')
-        x1, y1, x2, y2 = landmarks_batch[i][2:6]
-        # print(i,x1,y1,x2,y2)
+    def forward(self, x):
+        x = F.elu(F.max_pool2d(self.conv1(x),2))
+        x = F.elu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))  
+        x = x.view(-1, num_param)
+        x = F.elu(self.fc1(x))
+        x = F.dropout(x, training = self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+    
+    
+    
+    
+### Global Methods 
 
-        plt.plot([x1,x2], [y1,y1], c='r', marker='o')
-        plt.plot([x1,x2], [y2,y2], c='r', marker='o')
-        plt.plot([x1,x1], [y1,y2], c='r', marker='o')
-        plt.plot([x2,x2], [y1,y2], c='r', marker='o')
-        # plt.pause(0.001)  # pause a bit so that plots are updated
+# This function trains the neural network for one epoch
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        
+        # Move the input and target data on the GPU
+        data, target = data.to(device), target.to(device)
+        # Zero out gradients from previous step
+        optimizer.zero_grad()
+        # Forward pass of the neural net
+        output = model(data)
+        # Calculation of the loss function
+        #loss = F.nll_loss(output, target)
+        loss = F.cross_entropy(output, target)
+        # Backward pass (gradient computation)
+        loss.backward()
+        # Adjusting the parameters according to the loss function
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {}| BatchIdx: {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx, batch_idx * len(data), len(train_loader)*BATCH_SIZE,
+                100. * batch_idx / len(train_loader), loss.item()))
+        
+        # loss per batch
+        train_loss[epoch][batch_idx] = loss
+        
+        
+    train_loss_epoch.append(np.mean(train_loss[epoch]))
+       
+    
+# validation 
+def validate():
+    model.eval()
+    val_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            #val_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            
+            # loss per validation batch
+            val_loss += F.cross_entropy(output, target, size_average=True)
+            
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+  
+            
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-        plt.title('Batch from dataloader')
+    val_loss /= len(val_loader)
+    list_val_loss.append(val_loss)
+    list_accuracy.append(100. * (correct / val_size))
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, val_size,
+        100. * (correct / val_size)))
 
-for i_batch, sample_batched in enumerate(dataloader):
-    print(i_batch, sample_batched['image'].size(),
-          sample_batched['landmarks'].size())
 
-    # observe 4th batch and stop.
-    if i_batch == 3:
-        plt.figure()
-        show_landmarks_batch(sample_batched)
-        plt.axis('off')
-        plt.ioff()
-        plt.show()
-        break
+
+
+### Transformation applied to Training Data
+
+data_transforms = transforms.Compose([
+        transforms.Resize((32,32)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5, 0.5, 0.5], [1, 1, 1])
+    ])
+
+
+image_dataset = datasets.ImageFolder(os.path.join(data_dir), data_transforms)
+dataset_size = len(image_dataset)
+
+
+training_size = int(0.8*dataset_size)
+
+val_size = dataset_size - training_size
+
+train_weights = np.concatenate([np.ones(training_size), np.zeros(val_size)])
+
+np.random.seed()
+np.random.shuffle(train_weights)
+
+val_weights = np.ones(len(train_weights)) - train_weights
+
+
+# used WeightedRandomSampler to split Dataset, courtesy of Michael Floßmann
+sample_splitter = {'Training': WeightedRandomSampler(train_weights, int(sum(train_weights)), replacement = False),
+                  'Validation' : WeightedRandomSampler(val_weights, int(sum(val_weights)), replacement = False)}
+
+
+
+print("Length of orig. training set: {}".format(dataset_size))
+print("Splitted into:")
+print("New training set of length {}".format(len(sample_splitter['Training'])))
+print("New validation set of length {}".format(len(sample_splitter['Validation'])))
+
+
+
+
+### Loader Objects
+train_loader = DataLoader(image_dataset, batch_size = BATCH_SIZE, shuffle = False, pin_memory = True,
+                             sampler = sample_splitter['Training'], drop_last = True)
+
+val_loader = DataLoader(image_dataset, batch_size = BATCH_SIZE, shuffle = False, pin_memory = True,
+                        sampler = sample_splitter['Validation'], drop_last = True)
+
+
+### Some sanity checks
+print("(SanityCheck) TrainLoader length: {}".format(len(train_loader)))
+print("(SanityCheck) ValidationLoader length: {}".format(len(val_loader)))
+print("val_loader.dataset length: {}".format(len(val_loader.dataset)))
+
+
+model = Net().to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.5)
+
+train_loss = np.zeros((num_train_epochs, len(train_loader)))
+
+print("Shape of train_loss array: {}".format(train_loss.shape))
+
+start = time.time()
+
+for epoch in range(0, num_train_epochs):
+    train(epoch)
+    validate()
+
+
+train_mean_loss = np.mean(train_loss, axis = 0)
+print(train_mean_loss.shape)
+                          
+                          
+
+end = time.time()
+print("Traning time: {:.2f} min".format((end - start)/60))
+
+
+
+# .csv savings
+np.savetxt("trainLoss_{}.csv".format(lr), train_loss_epoch, delimiter=',')
+np.savetxt("valiLoss_{}.csv".format(lr), list_val_loss, delimiter=',')
+
+
+print("Train Loss list length: {}".format(len(train_loss)))                                            
+print("Test Loss list length: {}".format(len(list_val_loss)))
+
+# plotting
+plt.figure()
+plt.title("Training vs Validation Losses with LR {}".format(lr))
+plt.plot(train_loss_epoch, label="Training Loss")
+plt.plot(list_val_loss, label="Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel('Epochs')
+ax1.set_ylabel('Loss', color='black')
+ax1.plot(train_loss_epoch, color='blue',label='Training Loss')
+ax1.plot(list_val_loss, color='orange', label='Validation Loss')
+
+ax1.tick_params(axis='y', labelcolor='black')
+
+plt.legend()
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+
+ax2.set_ylabel('Accuracy', color='black')  # we already handled the x-label with ax1
+ax2.plot(list_accuracy, color='red', label='Validation Accuracy')
+ax2.tick_params(axis='y', labelcolor='black')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+
+
+plt.legend()
+
+plt.show()
+        
