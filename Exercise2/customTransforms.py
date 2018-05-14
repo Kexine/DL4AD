42804b15416dec801958b12f514505545b7f1a86
Michael Flossmannmarroles <flosmanm@tfpool54.informatik.uni-freiburg.de>
2018-05-14 11:36:42 +0200

    Salt and pepper works now

--- Exercise2/customTransforms.py
+++ Exercise2/customTransforms.py
@@ -22,17 +22,14 @@ class SaltNPepper(object):
     def __call__(self, img):
         shape = img.shape
 
-        # TODO: make this work
+        salt_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
 
-        salt_mask = torch.trunc(self.amount_salt*torch.ones(shape,
-                                                            dtype=torch.dtype.uint8) + torch.rand(shape))
-        # pepper_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+        pepper_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
 
         # apply salt
-        img.masked_scatter_(torch.ByteTensor(salt_mask), salt_mask)
+        img.masked_scatter_(salt_mask.byte(), salt_mask)
 
         # apply pepper
-        # img.masked_scatter_(torch.ByteTensor(pepper_mask), torch.ones(shape) - pepper_mask)
+        img.masked_scatter_(pepper_mask.byte(), torch.ones(shape) - pepper_mask)
 
         return img
-
