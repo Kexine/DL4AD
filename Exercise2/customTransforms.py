01da422d454b8443cde8bec25e54dbc624946251
Michael Flossmannmarroles <flosmanm@tfpool54.informatik.uni-freiburg.de>
2018-05-14 16:06:41 +0200

    made salt_n_pepper black and white

--- Exercise2/customTransforms.py
+++ Exercise2/customTransforms.py
@@ -10,7 +10,7 @@ class SaltNPepper(object):
     integer: both salt and pepper equally added
     tuple: (salt, pepper)-amount
     """
-    def __init__(self, fraction):
+    def __init__(self, fraction, colored=False):
         if isinstance(fraction, tuple):
             self.amount_salt = fraction[0]
             self.amount_pepper = fraction[1]
@@ -18,13 +18,25 @@ class SaltNPepper(object):
             self.amount_salt = fraction
             self.amount_pepper = fraction
 
+        self.colored = colored
+
 
     def __call__(self, img):
         shape = img.shape
 
-        salt_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+        # if the noise should be black and white: copy one mask to all colourchannels
+        if self.colored:
+            salt_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+            pepper_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+        else:
+            shape = (1, shape[1], shape[2])
+            salt_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+            pepper_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+            # we need to expand the maps to every channel
+            salt_mask = torch.cat((salt_mask,salt_mask,salt_mask), dim=0)
+            pepper_mask = torch.cat((pepper_mask,pepper_mask,pepper_mask), dim=0)
 
-        pepper_mask = torch.trunc(self.amount_salt*torch.ones(shape) + torch.rand(shape))
+        print("Shape mask: {}, Shape img: {}".format(salt_mask.shape, img.shape))
 
         # apply salt
         img.masked_scatter_(salt_mask.byte(), salt_mask)
