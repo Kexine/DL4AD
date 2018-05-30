72daa0fa536b52f520e287fee76f6b7aaf894936
Michael Flossmannmarroles <flosmanm@tfpool52.informatik.uni-freiburg.de>
2018-05-30 19:40:44 +0200

    fixed the h5Dataloader

--- src/extract.py
+++ src/extract.py
@@ -149,39 +149,42 @@ class H5Dataset(Dataset):
         file_idx = int(idx / IMAGES_PER_FILE)
         idx = idx % IMAGES_PER_FILE
 
-        # print("Idx: {}, Type: {}".format(idx, type(idx)))  # TODO: Remove me
         f = h5py.File(self.root_dir + '/' + self.file_names[file_idx], 'r')
 
         # for magic idx numers inspect class description
         data = f['rgb']
         targets = f['targets']
-        sample = {'filename' : self.file_names[file_idx],
-                  'data' : data[idx], 
-                  'targets' : targets[idx]}
+        sample = (data[idx],
+                  targets[idx])
 
         if self.transform:
-            sample['data'] = self.transform(sample['data'])
+            sample = (self.transform(data[idx]),
+                      torch.Tensor(targets[idx]))
+        else:
+            sample = (data[idx],
+                      targets[idx])
+
         return sample
 
-    
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #8 layers if con layers for image module
         self.conv1 = nn.Conv2d(3, 32, kernel_size=5, padding=0,stride =2) #(output = 100x44)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)#(output = 100x44)   
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)#(output = 100x44)
         self.conv3= nn.Conv2d(32, 64, kernel_size=3, padding=1,stride =2) #(output = 50x22)
         self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)#(output = 50x22)
         self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1,stride =2)#(output = 25x11)
-        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)#(output = 25*11)   
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)#(output = 25*11)
         self.conv7 = nn.Conv2d(128, 256, kernel_size=3, padding=1)#(output = 25*11)
         self.conv8 = nn.Conv2d(256, 256, kernel_size=3, padding=1)#(output = 25*11)
-        
+
         #defining 2 different dropouts required after the conv and fc layers
         self.conv_drop = nn.Dropout2d(p=0.2)
         self.fc_drop = nn.Dropout2d(p=0.5)
-        
-        
+
+
         #batch normalisers for every convolution layer
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2_bn = nn.BatchNorm2d(32)
@@ -191,131 +194,132 @@ class Net(nn.Module):
         self.conv6_bn = nn.BatchNorm2d(128)
         self.conv7_bn = nn.BatchNorm2d(256)
         self.conv8_bn = nn.BatchNorm2d(256)
-                                        
-        
+
+
         #2 fc layers for image module
         # self.fc1 = nn.Linear(204*92*256, 512) #(please reconfirm with team)
         self.fc1 = nn.Linear(25*11*256, 512)
         self.fc2 = nn.Linear(512, 512)
-        
+
         #3 fc layers for control and measurement modules
         self.fc3= nn.Linear(1,128)
         self.fc4= nn.Linear(128,128)
-        
+
         #4 fc layers for concatenated module
         self.fc5= nn.Linear(768,512)
         self.fc6= nn.Linear(512,256)
         self.fc7= nn.Linear(256,256)
-        
+
         #5 for action output
         self.fc8= nn.Linear(256,2)
-        
-        
+
+
     def forward(self, x,m,c):
-        
+
         #######conv layers##############
         x = self.conv1(x)
         x = self.conv1_bn(x)
         x = self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv2(x)
         x= self.conv2_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv3(x)
         x= self.conv3_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv4(x)
         x= self.conv4_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv5(x)
         x= self.conv5_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv6(x)
         x= self.conv6_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv7(x)
         x= self.conv7_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         x= self.conv8(x)
         x= self.conv8_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-        
+
         ###################################
-        
+
         # x = x.view(-1, 204*92*256)      ### TODO: change this
         x = x.view(-1, 25*11*256)
-        
+
         #########fully connected layers####
         x = self.fc1(x)
         x = self.fc_drop(x)
         x = relu(x)
-        
+
         x = self.fc2(x)
         x = self.fc_drop(x)
         x = relu(x)
-        
-        
+
+
         ####################################
-        
+
         ####for  measurement(speed)#########
         m = self.fc3(m)
         x= self.fc_drop(x)
         x = relu(x)
-        
+
         m = self.fc4(m)
         m = self.fc_drop(m)
         m = relu(m)
         ####################################
-        
+
         #########for control################
         c = self.fc3(c)
         c = self.fc4(c)
-        
+
         ###concatenating previous layers####
         j = torch.cat((x,m,c), 1)
         j = self.fc5(j)
         j = self.fc_drop(j)
         j = relu(j)
-        
+
         ####################################
         j = self.fc6(j)
         j= self.fc_drop(j)
         j = relu(j)
-        
+
         j = torch.cat((x,m,c), 1)
         j = self.fc7(j)
         j = self.fc_drop(j)
         j = relu(j)
-        
+
         #### output action##########
         action = self.fc8(j)
-        
+
         return action
-    
+
 
 def train(epoch, train_loader):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        print('type data: {}, type target: {}'.format(type(data), type(target)))
         # Move the input and target data on the GPU
         data, target = data.to(device), target.to(device)
         # Zero out gradients from previous step
         optimizer.zero_grad()
         # Forward pass of the neural net
-        output = model(data, 
+        output = model(data,
                        target[SPEED_IDX],
                        target[HIGH_LEVEL_COMMAND_IDX])
         # Calculation of the loss function
@@ -346,14 +350,14 @@ if  __name__=="__main__":
     un_composed = transforms.Compose([transforms.ToTensor()])
 
 
-    train_set = H5Dataset(root_dir = 'Agenthuman', transform=un_composed)
+    train_set = H5Dataset(root_dir = '../data/AgentHuman/SeqTrain', transform=un_composed)
     train_loader = torch.utils.data.DataLoader(train_set,
-                                               batch_size=64, 
-                                               shuffle=True, 
+                                               batch_size=64,
+                                               shuffle=True,
                                                pin_memory=False)
-    
-    orig_train_set = H5Dataset(root_dir = 'Agenthuman', transform=un_composed)
-    
+
+    orig_train_set = H5Dataset(root_dir = '../data/AgentHuman/SeqTrain', transform=un_composed)
+
     model = Net().to(device)
 
     relu = F.relu
@@ -363,7 +367,7 @@ if  __name__=="__main__":
     # criterion = nn.CrossEntropyLoss()
 
     lossx = []
-    
+
     num_train_epochs = 1
     for epoch in range(1, num_train_epochs + 1):
         train(epoch, train_loader)
