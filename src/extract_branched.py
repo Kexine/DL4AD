0a2c37133321f22b4259438ac619d564a0184124
Kshitij Sirohi <sirohik@tfpool63.informatik.uni-freiburg.de>
2018-06-02 18:50:20 +0200

    Branched Structure Compatible with our changes v1.0

--- src/extract_branched.py
+++ src/extract_branched.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -172,6 +171,7 @@ class H5Dataset(Dataset):
 
 
 class Net(nn.Module):
+    branches = []
     def __init__(self):
         super(Net, self).__init__()
         #8 layers if con layers for image module
@@ -183,11 +183,13 @@ class Net(nn.Module):
         self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)#(output = 25*11)
         self.conv7 = nn.Conv2d(128, 256, kernel_size=3, padding=1)#(output = 25*11)
         self.conv8 = nn.Conv2d(256, 256, kernel_size=3, padding=1)#(output = 25*11)
-
+        
+        
         #defining 2 different dropouts required after the conv and fc layers
         self.conv_drop = nn.Dropout2d(p=0.2)
         self.fc_drop = nn.Dropout2d(p=0.5)
-
+        
+        
         #batch normalisers for every convolution layer
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv2_bn = nn.BatchNorm2d(32)
@@ -197,121 +199,116 @@ class Net(nn.Module):
         self.conv6_bn = nn.BatchNorm2d(128)
         self.conv7_bn = nn.BatchNorm2d(256)
         self.conv8_bn = nn.BatchNorm2d(256)
-
+                                        
+        
         #2 fc layers for image module
-        # self.fc1 = nn.Linear(204*92*256, 512) #(please reconfirm with team)
         self.fc1 = nn.Linear(25*11*256, 512)
         self.fc2 = nn.Linear(512, 512)
-
+        
         #3 fc layers for control and measurement modules
         self.fc3= nn.Linear(1,128)
         self.fc4= nn.Linear(128,128)
-
+        
         #4 fc layers for concatenated module
-        self.fc5= nn.Linear(768,512)
+        self.fc5= nn.Linear(640,512)
         self.fc6= nn.Linear(512,256)
         self.fc7= nn.Linear(256,256)
-
+        
         #5 for action output
         self.fc8= nn.Linear(256,2)
-
-
-    def forward(self, x,m,c):
-
+        self.fc9= nn.Linear(256,1)
+        
+        
+    def forward(self, x,speed):
         #######conv layers##############
-        x = self.conv1(x)
-        x = self.conv1_bn(x)
-        x = self.conv_drop(x)
+        x= self.conv1(x)
+        x= self.conv1_bn(x)
+        x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv2(x)
         x= self.conv2_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv3(x)
         x= self.conv3_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv4(x)
-        x= self.conv4_bn(x)
+        x= self.conv4_bn(x) 
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv5(x)
         x= self.conv5_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv6(x)
         x= self.conv6_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv7(x)
         x= self.conv7_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         x= self.conv8(x)
         x= self.conv8_bn(x)
         x= self.conv_drop(x)
         x = relu(x)
-
+        
         ###################################
-
-        # x = x.view(-1, 204*92*256)      ### TODO: change this
-        print("Shape of x before view(): {}".format(x.shape))
-        x = x.view(-1, 25*11*256)
-
+        
+        x = x.view(-1, 25*11*256)      ### do change this
+        
         #########fully connected layers####
         x = self.fc1(x)
-        x = self.fc_drop(x)
+        x= self.fc_drop(x)
         x = relu(x)
-
+        
         x = self.fc2(x)
-        x = self.fc_drop(x)
+        x= self.fc_drop(x)
         x = relu(x)
-
-
-        ####################################
-
+        
+        #####do something for control########
+        
         ####for  measurement(speed)#########
-        m = m.view(m.shape[0], -1)
-        m = self.fc3(m)
-        m= self.fc_drop(m)
-        m = relu(m)
-
-        m = self.fc4(m)
-        m = self.fc_drop(m)
-        m = relu(m)
+        ###not to use in real raiscar#######
+        speed = speed.view(speed.shape[0], -1) 
+        speed = self.fc3(speed)
+        speed= self.fc_drop(speed)
+        speed = relu(speed)
+        
+        speed = self.fc4(speed)
+        speed= self.fc_drop(speed)
+        speed = relu(speed)
         ####################################
-
-        #########for control################
-        c = c.view(c.shape[0], -1)
-        c = self.fc3(c)
-        c = self.fc4(c)
-
-        ###concatenating previous layers####
-        j = torch.cat((x,m,c), 1)
+        
+        ####################################
+        j = torch.cat((x,speed), 1)
         j = self.fc5(j)
-        j = self.fc_drop(j)
-        j = relu(j)
-
-        ####################################
-        j = self.fc6(j)
         j= self.fc_drop(j)
         j = relu(j)
-
-        j = self.fc7(j)
-        j = self.fc_drop(j)
-        j = relu(j)
-
+        
+        ####initiating branches############
+        branch_config = [["Steer", "Gas"],["Steer", "Gas"], ["Steer", "Gas"],["Steer", "Gas"]]
+        ###there were 5 in the code they made, dontn have idea why####
+        branches=[]
+        for i in range(0, len(branch_config)):
+            branch_output = self.fc6(j)
+            branch_output= self.fc_drop(branch_output)
+            branch_output = relu(branch_output)
+            branch_output = self.fc7(branch_output)
+            branch_output= self.fc_drop(branch_output)
+            branch_output = relu(branch_output)
+            branches.append(self.fc8(branch_output))
+        #have to look for this regarding the dataset , on how to use it?
         #### output action##########
-        action = self.fc8(j)
-
-        return action
+        return branches
 
 
 def train(epoch, train_loader):
@@ -324,17 +321,29 @@ def train(epoch, train_loader):
         # Zero out gradients from previous step
         optimizer.zero_grad()
         # Forward pass of the neural net
-        output = model(data,
-                       target[:,SPEED_IDX],
-                       target[:,HIGH_LEVEL_COMMAND_IDX])
-        print(output.shape)
+        output_branches = model(data,
+                                target[:,SPEED_IDX])
+        print(output_branches[0].shape)
         # Calculation of the loss function
-        output_target = target[:,:2]  # TODO: remove magic numbers
-        loss = nn.MSELoss()(output.double(), output_target.double())
-        # Backward pass (gradient computation)
-        loss.backward()
-        # Adjusting the parameters according to the loss function
-        optimizer.step()
+        for c in range(0,len(target[0].shape)):
+            
+            output_target = target[:,:2]# TODO: remove magic numbers
+            
+            print("Shape of x before view(): {}".format(output_branches[0].shape))
+            
+            if target[c,HIGH_LEVEL_COMMAND_IDX] == 2:
+                output = output_branches[0]
+            if target[c,HIGH_LEVEL_COMMAND_IDX] == 3:
+                output = output_branches[1]
+            if target[c,HIGH_LEVEL_COMMAND_IDX] == 4:
+                output = output_branches[2]
+            if target[c,HIGH_LEVEL_COMMAND_IDX] == 5:
+                output = output_branches[3]
+            loss = nn.MSELoss()(output.double(), output_target.double())
+            # Backward pass (gradient computation)
+            loss.backward()
+            # Adjusting the parameters according to the loss function
+            optimizer.step()
         if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
