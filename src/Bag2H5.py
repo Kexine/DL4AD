44da37b806be337237b277f69fc1777cc2a84c31
Hendrik <hendrik.vloet@gmx.de>
2018-07-06 21:13:06 +0200

    added rolling average with adjustable window size

--- src/Bag2H5.py
+++ src/Bag2H5.py
@@ -30,7 +30,6 @@ AXIS_RIGHT_STICK = 3
 
 
 
-
 '''
 Due to random camera initialization of ROS, we have two camera topic setups:
 1. Train Bag:
@@ -163,6 +162,9 @@ def main():
     parser.add_argument("--only_middle",
                         help="Set to true to only get middle camera images",
                         action='store_true')
+    parser.add_argument("-w","--window",
+                        help="Set size of rolling window for steering smoothing",
+                        default=10)
 
     args = parser.parse_args()
     destination = args.destination
@@ -174,6 +176,7 @@ def main():
     ONLY_MIDDLE = args.only_middle
 
     STEERING_OFFSET = 0.45
+    TRACE_SIZE = int(args.window)
 
     # create directories and return paths
     middle_destination, right_destination, left_destination = make_dirs(destination,ENABLE_TEST_BAG)
@@ -210,6 +213,9 @@ def main():
     file_cnt_r = 0
     file_cnt_l = 0
 
+    ctrl_idx = 0
+    steer_trace = np.zeros(TRACE_SIZE)
+
     bar = progressbar.ProgressBar(max_value = bag.get_message_count())
 
     for idx, (topics, msg, t) in enumerate(bag.read_messages(start_time=start_time)):
@@ -236,7 +242,13 @@ def main():
             dir_string = COMMAND_DICT[command]
 
             # turning left is positive, turning right is negative
-            analog_steer = current_buttons.axes[AXIS_LEFT_STICK]
+            steer_trace[ctrl_idx % TRACE_SIZE ] =  current_buttons.axes[AXIS_LEFT_STICK]
+            ctrl_idx += 1
+
+            # analog_steer = current_buttons.axes[AXIS_LEFT_STICK]
+
+
+
             # left_stick_up_down = current_buttons.axes[1]
 
             # right_stick_left_right =  current_buttons.axes[2]
@@ -267,7 +279,11 @@ def main():
             middle_image_original = msg_to_mat(msg)
             rescaled_image = rescale(middle_image_original)
 
-            # save middle cam information
+            analog_steer = np.mean(steer_trace)
+
+            # print("steer trace {}".format(steer_trace))
+            # print("averaged steer {}".format(analog_steer))
+            # print("ctrl index {}".format(ctrl_idx))
 
             if math.isnan(command):
                 f_m["targets"][cnt_middle] = float('nan')
@@ -308,6 +324,8 @@ def main():
                 rescaled_image = rescale(right_image_original)
                 cmp_cmd = get_complementary_cmd(topics, command)
 
+                analog_steer = np.mean(steer_trace)
+
                 if math.isnan(cmp_cmd):
                     f_r["targets"][cnt_right] = float('nan')
                 else:
@@ -340,6 +358,8 @@ def main():
                 rescaled_image = rescale(left_image_original)
 
                 cmp_cmd = get_complementary_cmd(topics, command)
+
+                analog_steer = np.mean(steer_trace)
 
                 if math.isnan(cmp_cmd):
                     f_l["targets"][cnt_left] = float('nan')
